6e13157987564e6d1332a998f8e7b26834c9599a
Lynx-Lynx <delinne.weird@gmail.com>
2021-08-28 00:58:53 +0300

    optimized the app, added color box

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 const button = document.getElementById("convert");
+const reset = document.getElementById("reset");
 const input = document.getElementById("userinput");
-const p1 = document.getElementById("p1");
+const result = document.getElementById("result");
+let colorBox = document.getElementById("colorBox");
 
 
-function inputLength() {						
+const inputLength = () => {						
 	return input.value.length;
 }
 
-function inputCheck() {
+const inputCheck = () => {
 	let commas = "";
 	for (let i = 0; i < input.value.length; i++){
 		if (input.value[i] === ",") {
 			commas += input.value[i];
 		}
 	}
-	
 	return commas.length;
 }
 
-function validCheck() {
+const validCheck = () => {
 	let inputArray = input.value.replace(/[() ]/g,'').split(",");
 		check = "";
 		for (element of inputArray) {
@@ -29,39 +30,65 @@ function validCheck() {
 	return check.length;
 }
 
-function hexToRgb() {			//converts HEX to RGB or vice verca
+const hexToRgb = () => {			//converts HEX to RGB or vice verca
 	let color = input.value.replace(/[() ]/g,'');
 	switch(true) {
 		case(color.startsWith("#") && color.length === 7):
 			let rgb = color.substring(1).match(/.{2}/g).map(a => parseInt(a, 16));
-			return !rgb.includes(NaN) ? `✓ Hex to RGB: rgb(${rgb})` : "Please enter valid Hex color code";
+			const setRgbColor = () => {
+				colorBox.style.backgroundColor = `rgb(${[...rgb]})`;
+				colorBox.style.visibility = "visible";
+				return `✓ Hex to RGB: rgb(${rgb})`; 
+			}
+			const throwRgbError = () => {
+				colorBox.style.visibility = "hidden";
+				return "Please enter valid Hex color code"; 
+			}
+			return !rgb.includes(NaN) ? setRgbColor() : throwRgbError(); 
 		case(/^\d/.test(color) && inputCheck() === 2 && validCheck() <= 3):
 			let hex = "#" + color.split(",").map(a => parseInt(a).toString(16).padStart(2, '0')).join('');
-			return hex.length !== 7 || hex.includes("NaN") ? "Please enter valid RGB color code (0...255)" : `✓ RGB to Hex: ${hex}`;
+			const setHexColor = () => {
+				colorBox.style.backgroundColor = hex;
+				colorBox.style.visibility = "visible";
+				return `✓ RGB to Hex: ${hex}`; 
+			}
+			const throwHexError = () => {
+				colorBox.style.visibility = "hidden";
+				return "Please enter valid RGB color code (0...255)"; 
+			}
+			return hex.length !== 7 || hex.includes("NaN") ? throwHexError() : setHexColor();
 		default:
+			colorBox.style.visibility = "hidden";
 			return "Please enter valid color code";				
 	}
-}
-										
+}							
 
-function showConverted() {
-		p1.textContent = hexToRgb();
+const showConverted = () => {
+	result.textContent = hexToRgb();
 }
 
-function addAfterClick() {			///[,\-]/.test(input.value)
+const addAfterClick = () => {
 	if (inputLength() > 0) {
 		showConverted();
-	}	
+	}
 }
 
-function addAfterKeyPress(event) {
+const addAfterKeyPress = event => {
 	if (inputLength() > 0 && event.keyCode === 13) {
 			showConverted();
 	}
 }
 
+const resetValue = () => {
+	input.value = "";
+	result.textContent = "";
+	colorBox.style.visibility = "hidden";
+}
+
 
 button.addEventListener("click", addAfterClick);
+
+reset.addEventListener("click", resetValue);
 
 input.addEventListener("click", addAfterClick);
 
